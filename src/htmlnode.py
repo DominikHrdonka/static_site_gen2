c31c1358b48dba17c51853ce10c1586f737b491c
Dominik Hrdonka <hrdonka.d@gmail.com>
2025-02-02 11:48:50 +0100

    create HTMLNode and test_htmlnode.py for unittests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,4 +9,16 @@ class HTMLNode():
         raise NotImplementedError
     
     def props_to_html(self):
-        pass
+        final_string = ""
+        for key in self.props:
+            final_string += " " + key + '="' + self.props[key] + '"'
+        return final_string
+    
+    ###eq method not really needed for unittests, if we want to test props_to_html methods - Python knows how to compare strings
+    def __eq__(self, other):
+        if not isinstance(other, HTMLNode):
+            return False
+        return self.tag == other.tag and self.value == other.value and self.children == other.children and self.props == other.props
+    
+    def __repr__(self):
+        return f"HTMLNode ({self.tag}, {self.value}, {self.children}, {self.props})"
