f4b1ea545ac29a9506f87240d561857588ee1225
Dominik Hrdonka <hrdonka.d@gmail.com>
2025-02-03 18:25:21 +0100

    write unit tests for LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     
@@ -23,3 +23,24 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode()
         string = ""
         self.assertEqual(node.props_to_html(), string)
+
+    #Testing LeafNode methods
+
+    def test_to_html_no_props(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        final_string = "<p>This is a paragraph of text.</p>"
+        self.assertEqual(node.to_html(), final_string)
+    
+    def test_to_html_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        final_string = '<a href="https://www.google.com">Click me!</a>'
+        self.assertEqual(node.to_html(), final_string)
+    
+    def test_to_html_no_value(self):
+        node = LeafNode("p", None)
+        self.assertRaises(ValueError, node.to_html)
+    
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Just a raw text.")
+        final_string = "Just a raw text."
+        self.assertEqual(final_string, node.to_html())
