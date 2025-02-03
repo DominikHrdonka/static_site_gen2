c5a7f013e6577ed3d0e13cc06d3e5201ab4a037c
Dominik Hrdonka <hrdonka.d@gmail.com>
2025-02-03 18:12:50 +0100

    create to_html method

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -27,3 +27,15 @@ class HTMLNode():
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
         super().__init__(tag, value, [], props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("All leaf nodes must have a value")
+
+        elif not self.tag:
+            return self.value
+        
+        else:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        
+        
