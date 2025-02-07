dccf29a9418ea1111b6c73117eb4bb772c0e30f4
Dominik Hrdonka <hrdonka.d@gmail.com>
2025-02-07 18:11:51 +0100

    write logic for split_node_delimiter function

--- src/inline.py
+++ src/inline.py
@@ -3,7 +3,16 @@ def split_node_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for node in old_nodes:
         if node.text_type == TextType.TEXT:
-            node.text.split(delimiter)
+            split_text = node.text.split(delimiter)
+            if len(split_text) % 2 == 0:
+                raise ValueError("Improper delimiter formatting.")
+            else:
+                for index, item in enumerate(split_text):
+                    if index % 2 == 1:
+                        new_nodes.append(TextNode(item, text_type))
+                    else:
+                        new_nodes.append(TextNode(item, TextType.TEXT))
+                        
         else:
-            new_nodes.extend(node)
+            new_nodes.append(TextNode(node.text, node.text_type))
         
